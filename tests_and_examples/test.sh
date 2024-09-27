e54aefd786d1c3cff6800d9735f930da94907fcc
zarquon-42 <zarquon@shupe.ca>
2024-09-26 23:48:53 -0700

    Some examples and tests

---
+++ tests_and_examples/test.sh
@@ -0,0 +1,17 @@
+python3 patch-installer.py patch-example-1.yml --dry-run --verbose
+echo 
+echo ======================================================================
+echo
+python3 patch-installer.py patch-example-2.yml --patch-id patch2 --dry-run --verbose
+echo 
+echo ======================================================================
+echo
+python3 patch-installer.py patch-example-3.yml --patch-id patch3 --dry-run --verbose
+echo 
+echo ======================================================================
+echo
+python3 patch-installer.py patch-example-multiple.yml --patch-id patch1 --dry-run --verbose
+echo 
+echo ======================================================================
+echo
+python3 patch-installer.py patch-example-multiple.yml --patch-id patch2 --dry-run --verbose
